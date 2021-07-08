98c4675fff22ef3e4be5149673f7823e77563a41
pguilimaf <pguilimaf@gmail.com>
2021-07-08 16:34:23 -0300

    Passando o Contato entre Telas

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -3,6 +3,8 @@ import 'dart:io';
 import 'package:agenda_de_contatos/helpers/contact_helper.dart';
 import 'package:flutter/material.dart';
 
+import 'contact_page.dart';
+
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
@@ -34,7 +36,9 @@ class _HomePageState extends State<HomePage> {
       ),
       backgroundColor: Colors.white,
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          _showContactPage();
+        },
         child: Icon(Icons.add),
         backgroundColor: Colors.red,
       ),
@@ -90,6 +94,19 @@ class _HomePageState extends State<HomePage> {
           ],
         ),
       )),
+      onTap: () {
+        _showContactPage(contact: contacts[index]);
+      },
+    );
+  }
+
+  void _showContactPage({Contact contact}) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+          builder: (context) => ContactPage(
+                contact: contact,
+              )),
     );
   }
 }
